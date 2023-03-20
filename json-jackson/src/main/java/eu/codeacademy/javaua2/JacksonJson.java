2300e6efdbbfeb2f75a95fa8c9227ebf5fcbfc02
Lxxtsch <robertas.laatsch@gmail.com>
2023-03-20 21:18:10 +0200

    moved logic from main to JsonExampleService class

--- json-jackson/src/main/java/eu/codeacademy/javaua2/JacksonJson.java
+++ json-jackson/src/main/java/eu/codeacademy/javaua2/JacksonJson.java
@@ -3,6 +3,7 @@ package eu.codeacademy.javaua2;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.codeacademy.javaua2.model.Car;
 import eu.codeacademy.javaua2.model.Person;
+import eu.codeacademy.javaua2.service.JsonExampleService;
 import eu.codeacademy.javaua2.utils.FileUtils;
 
 import java.io.File;
@@ -17,26 +18,10 @@ public class JacksonJson
 {
     public static void main( String[] args ) throws IOException, URISyntaxException {
 
-        ////////// Saving to file
+        JsonExampleService example = new JsonExampleService();
 
-        Person person = new Person("Petras", 25);
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.writeValue(new File("target/person.json"), person);
-
-        /////////// Reading from file
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        File file = new File("target/person.json");
-
-        Person personFromJson = mapper.readValue(file, Person.class);
-        System.out.println(personFromJson);
-
-        /////////// Using Get from resources class method
-        File carFile = FileUtils.getFileFromResource("car.json");
-
-        Car carFromJson = mapper.readValue(carFile, Car.class);
-        System.out.println(carFromJson);
+        example.basicSerializeAndDeserializeExample();
+        example.basicDeserializationFromResources();
 
     }
 }
